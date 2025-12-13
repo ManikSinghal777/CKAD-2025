757fdcca2fd601c988da11bb7b0510911c901337
ManikSinghal777 <148306981+ManikSinghal777@users.noreply.github.com>
2025-12-13 06:31:59 +0530

    Update Script.sh

--- capture pod logs | metrics troubleshooting/Script.sh
+++ capture pod logs | metrics troubleshooting/Script.sh
@@ -2,6 +2,35 @@
 
 #!/bin/bash
 
+# Ensure directory exists
+mkdir -p /opt/ckadnov2025
+
+# Create winter.yaml with required YAML content
+cat <<EOF > /opt/ckadnov2025/winter.yaml
+apiVersion: v1
+kind: Pod
+metadata:
+  name: winter
+spec:
+  containers:
+  - name: winter
+    image: busybox
+    command: ["sh", "-c", "echo Hello from winter pod && sleep 3600"]
+EOF
+
+# Create empty log_Output.txt (or overwrite if already exists)
+echo "" > /opt/ckadnov2025/log_Output.txt
+
+echo "Files created:"
+echo " - /opt/ckadnov2025/winter.yaml"
+echo " - /opt/ckadnov2025/log_Output.txt"
+
+-------
+
+#Task2
+
+#!/bin/bash
+
 echo "[+] Creating namespace cpu-stress..."
 kubectl create namespace cpu-stress --dry-run=client -o yaml | kubectl apply -f -
 
